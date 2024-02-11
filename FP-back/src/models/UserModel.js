685a0e71f9a19c98052486466e608af1620f9086
Qulamali <z.qulamali@gmail.com>
2024-02-12 01:36:12 +0400

    add

--- FP-back/src/models/UserModel.js
+++ FP-back/src/models/UserModel.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const UserSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    name: { type: String, required: true },
+    name: { type: String, required: true ,unique:true},
     phone: { type: Number },
     photo: { type: String },
     role: {
